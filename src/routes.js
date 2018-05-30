ad89c7344a65bd926c58f924b845ff850e9cf059
sazukii <pacootjelee3110@gmail.com>
2018-05-30 09:41:38 +0200

    commit

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import * as $ from 'jquery';
  * @param remoteserver
  * @returns {Promise}
  */
+
 const getroutesjson = (remoteserver) => {
     return new Promise((resolve, reject) => { //New promise for array
         // let routesjson = [];
@@ -25,16 +26,6 @@ const getroutesjson = (remoteserver) => {
     });
 };
 
-
-// let fetch = fetch('')
-//     .then(
-//         function (response) {
-//             if (response.status !==200){
-//                 console.log('er was een probleem bij de upload code:' + response.status);
-//                 return;
-//             }
-//             response.json().then(function (data) {
-//                 console.log(data);
 /**
  * Post a textfile to the remoteserver
  * @param remoteserver
